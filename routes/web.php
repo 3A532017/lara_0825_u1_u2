1a112307020d3d9ec3d27ddad16df9a6bcb99bb1
3A532017 <s3a532017@student.ncut.edu.tw>
2018-11-21 15:13:10 +0800

    練習2-2

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+\App\Post::create([
+    'title'=>'test title',
+    'content'=>'test content',
+]);
 
 Route::get('/', function () {
     return view('welcome');
