5d13d25bf0828e08854ee9efed92bea922e315a5
3A532017 <s3a532017@student.ncut.edu.tw>
2018-11-21 15:55:45 +0800

    練習6-1取得Collection

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,12 @@ Route::get('/', function () {
 /*    $post =\App\Post::find(4);
     $post->delete();
 */
-    \App\Post::destroy(3,5,7);
+//    \App\Post::destroy(3,5,7);
 
+    $allPosts =\App\Post::all();
+    dd($allPosts);
+    $featuredPosts =\App\Post::where('is_feature',1)->get();
+    dd($featuredPosts);
 
     return view('welcome');
 });
